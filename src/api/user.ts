774dcc72525d5f2b516942970fa226109fa86e80
Tanner <tcamp022@gmail.com>
2023-04-18 01:03:18 -0400

    Update user.ts

--- src/api/user.ts
+++ src/api/user.ts
@@ -7,7 +7,7 @@ export type User = {
   email: string;
   current_team_id: number;
   teams: Array<Team>;
-  ownedTeams: Array<Team>;
+  owned_teams: Array<Team>;
   avatar_url: string;
   uses_two_factor_authentication: boolean;
   is_sandboxed: boolean;
